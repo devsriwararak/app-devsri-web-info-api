14848771f48da7e1e97624685c93a2d429dc347c
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2023-12-16 16:35:10 +0700

    16-12-2566-v-2

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
+const express = require("express");
 
+const mysql = require("mysql2");
 
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
-})
+const app = express();
+const PORT = 4000;
 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "147.50.231.19",
+  user: "devsriwa_web_info",
+  password: "*Nattawut1234",
+  database: "devsriwa_web_info",
+});
+
+app.get("/home", (req, res) => {
+  res.status(200).json("Welcome, your app is working well");
+});
+
+app.get("/api", (req, res) => {
+  connection.query("SELECT * FROM `test` ", function (err, results, fields) {
+    res.json(results);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
 
 // Export the Express API
-module.exports = app
+module.exports = app;
